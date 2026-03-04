8c38cb52f40130f876d7f9396654b300e7d4f390
Rintu Chowdory <chowdoryrintu60@gmail.com>
2026-03-04 19:14:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8057266048-ca3e4",
+  "appId": "1:372398803509:web:17b2d6bfeed3e201f33f7c",
+  "apiKey": "AIzaSyDh4EwvN2O1EXhPqJpNocEMNSiuGziQ0lc",
+  "authDomain": "studio-8057266048-ca3e4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "372398803509"
+};
